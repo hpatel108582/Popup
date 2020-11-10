f6f96264b8fc937dae58689109ee15757bf19643
EC2 Default User <ec2-user@ip-172-31-17-230.ec2.internal>
2020-11-10 00:56:14 +0000

    pushed files

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 import os
 import flask
+import flask_socketio
+import json
 
 app = flask.Flask(__name__)
+
+
+socketio = flask_socketio.SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
 
 @app.route('/')
 def hello():
     return flask.render_template('index.html')
 
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8080)),
-    debug=True
-)
+
+@socketio.on('output')
+def on_new_number(data):
+    
+    print( data)
+    title = data['title']
+    start=data['start']
+    end=data['end']
+
+if __name__ == '__main__': 
+    socketio.run(
+        app,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080)),
+        debug=True
+    )
--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,7 +1,108 @@
-import * as React from 'react';
+import  React, { useState } from 'react';
+import Modal from 'react-modal';
+import TimePicker from 'rc-time-picker';
+import ReactDOM from 'react-dom';
+import 'rc-time-picker/assets/index.css';
+import moment from 'moment';
+import { Socket } from './Socket';
 
-export class Content extends React.Component {
-    render() {
-        return <div><h1>Hello World from React!</h1></div>;
+
+export function Content() {
+    const [modal, setModal] = React.useState(0);
+    const format = 'h:mm a';
+    const [sTime, setSTime] = useState('')
+    const [eTime, setETime] = useState('')
+    const [state,setState] = React.useState(
+        {
+            title: '',
+        
+        }
+    )
+    const now = moment().hour(0).minute(0);
+
+function onSChange(value) {
+ 
+  console.log(value.format(format));
+  setSTime(value.format(format))
+  
+  Socket.emit('output', {
+        'title': 'yo',
+    });
+} 
+
+function onEChange(value) {
+  console.log(value && value.format(format));
+  setETime( value.format(format))
+}
+    
+    
+    function handleChange(evt) {
+  const value = evt.target.value;
+  setState({
+    ...state,
+    [evt.target.name]: value
+  });
+}
+
+  const onMessageSubmit = (e) => {
+      e.preventDefault()
+      const {title } =state
+     
+      
+      
     }
+    
+   
+
+    return (
+        <div>
+            <button onClick={()=> setModal(true)}> Open Modal </button> 
+            <Modal  
+                isOpen={modal}
+                onRequestClose={()=>setModal(false)}
+            >
+            
+          <form onSubmit={onMessageSubmit}>
+      
+        <h1>Add Event</h1>
+        <input
+          type="text"
+          name="title"
+          value={state.title}
+          onChange={handleChange}
+        />
+      
+     
+        
+        <h3> Start Time </h3>
+            <TimePicker 
+                showSecond={false}
+                defaultValue={now}
+                className="xxx"
+                onChange={onSChange}
+                format={format}
+                use12Hours
+                inputReadOnly
+            /> 
+            <h3> End  Time </h3>
+            <TimePicker 
+                showSecond={false}
+                defaultValue={now}
+                className="xxx"
+        
+                onChange={onEChange}
+                format={format}
+                use12Hours
+                inputReadOnly
+            /> 
+      <button type="submit" onClick={()=> setModal(false)}>Send Event </button>
+    </form>
+    
+   
+            </Modal>
+        <p> {state.title} </p>
+        <p> {sTime} </p> 
+        <p> {eTime} </p>
+        </div>
+    );
 }
